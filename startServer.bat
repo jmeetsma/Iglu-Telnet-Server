32ca662f1e213c38aa774779a71277d3052f3e74
Jeroen Meetsma <jeroen@ijsberg.nl>
2012-02-07 20:18:59 +0100

    ...

--- startServer.bat
+++ startServer.bat
@@ -1,9 +1,9 @@
 @echo off
 
-SET CLASSPATH=lib\iglu-1.0.2.jar
-SET CLASSPATH=%CLASSPATH%;lib\iglu-util-0.9.0.jar
-SET CLASSPATH=%CLASSPATH%;lib\iglu-common-0.9.1.jar
-SET CLASSPATH=%CLASSPATH%;lib\iglu-telnet-server-1.0.jar
+SET CLASSPATH=lib\iglu-1.0.3.jar
+SET CLASSPATH=%CLASSPATH%;lib\iglu-util-0.9.1.jar
+SET CLASSPATH=%CLASSPATH%;lib\iglu-common-0.9.2.jar
+SET CLASSPATH=%CLASSPATH%;lib\iglu-telnet-server-1.0.1.jar
 
 @echo on
 java -classpath %CLASSPATH% -Xms64m -Xmx256m org.ijsberg.iglu.samples.telnet.StandaloneServerConfiguration
